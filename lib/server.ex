f982321eb5d063de83d410222ef80eae8e495ac1
solar05 <artem00298@gmail.com>
2024-10-12 03:52:12 +0300

    Remove obsolete

--- lib/server.ex
+++ lib/server.ex
@@ -1,6 +1,6 @@
 defmodule Server do
   @moduledoc """
-  Your implementation of a Redis server
+  Implementation of a Redis server
   """
 
   use Application
@@ -13,11 +13,6 @@ defmodule Server do
   Listen for incoming connections
   """
   def listen() do
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    # IO.puts("Logs from your program will appear here!")
-
-    # Uncomment this block to pass the first stage
-    #
     # Since the tester restarts your program quite often, setting SO_REUSEADDR
     # ensures that we don't run into 'Address already in use' errors
     {:ok, socket} = :gen_tcp.listen(6379, [:binary, active: false, reuseaddr: true])
